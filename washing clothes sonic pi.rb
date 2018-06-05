d97085aac78d4d51c13f074bf1da82a117c53897
joshua Gwinn <jdgwinn911@gmail.com>
2018-06-05 17:56:55 -0400

    this is the homework for the 1st day of class

--- washing clothes sonic pi.rb
+++ washing clothes sonic pi.rb
@@ -1,6 +1,11 @@
 def wear(smell)
-  sample :drum_tom_hi_hard, rate: smell
-  sleep 1
+  if smell >= 10
+    sample :drum_snare_hard
+  else
+    sample :drum_tom_hi_hard, rate: smell
+    sleep 0.1
+    wear(smell + 1)
+  end
 end
 
 def wash(temp)
@@ -8,13 +13,53 @@ def wash(temp)
   sleep 1
 end
 
-def dry
-  sample :drum_cymbal_hard
+def dry(dryer)
+  sample dryer
   sleep 1
 end
 
-5.times do
-  wear(4)
-  wash(70)
-  dry
+def iron
+  use_synth :growl
+  play 60
+  sleep 1
+end
+
+def dirty?
+  1 == [1,2].choose
+end
+
+def treat_stain
+  sample :ambi_choir
+  sleep 2
+end
+
+live_loop :laundry_1 do
+  sync :tick
+  wear(1)
+  treat_stain if dirty?
+  wash(90)
+  dry(:guit_harmonics)
+  iron
+end
+
+
+live_loop :laundry_2 do
+  sync :tick
+  wear(5)
+  treat_stain if dirty?
+  wash(90)
+  dry(:elec_cymbal)
+end
+
+
+live_loop :time do
+  sync :tick
+  sample :drum_bass_hard
+  sleep 0.5
+  sample :drum_snare_hard
+  sleep 0.5
+end
+
+live_loop :tick do
+  sleep 1
 end
